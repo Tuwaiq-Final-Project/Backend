cc50a538c831029d1f22a6a4c922e3f551c7a5b0
S-MW <72529306+S-MW@users.noreply.github.com>
2021-12-12 17:20:54 +0300

    Create spring boot project

---
+++ src/test/java/com/example/barbertime/BarberTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barbertime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
